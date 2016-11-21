dc85c96a896194829eac1003f3ab8e3ae257e8fc
Bindia Kalra <bindia.kalra@columbia.edu>
2016-11-20 22:33:55 -0500

    adding FB login

---
+++ tap_fashion/webapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'webapp'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^dashboard/$', views.DashboardView.as_view(), name='dashboard'),
+]
--- tap_fashion/webapp/views.py
+++ tap_fashion/webapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    """
+    Creates the view for the home page of the app
+    """
+    template_name = 'webapp/index.html'
+
+    def get_queryset(self):
+        return True
+
+
+
+class DashboardView(generic.ListView):
+    """
+    Creates the view for the dashboard page of the app
+    """
+    template_name = 'webapp/dashboard.html'
+
+    def get_queryset(self):
+        return True
